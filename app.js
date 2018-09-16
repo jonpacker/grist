55af0060f56def010af4e52eb1a09c5c2e553a45
Jon Packer <contact@jonpacker.com>
2018-09-16 21:37:44 +0200

    upgrade app.js

--- app.js
+++ app.js
@@ -1,38 +1,28 @@
+const express = require('express')
+const routes = require('./routes')
+const http = require('http')
+const path = require('path')
+const bodyParser = require('body-parser')
+const dev = process.env.NODE_ENV === 'development'
 
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes')
-  , http = require('http')
-  , path = require('path');
-
-var app = express();
+const app = express()
 
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/public'));
-app.use(express.static(path.join(__dirname, 'public')));
+if (dev) app.use(require('morgan')('tiny'))
+app.set('port', process.env.PORT || 3000)
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+app.use(bodyParser.json())
+app.use(app.router)
+app.use(require('stylus').middleware(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+app.put('/', routes.replaceRecipe)
+app.post('/', routes.importBeerXml)
+app.get('/:slug', routes.readRecipe)
+app.del('/:slug', routes.deleteRecipe)
+app.get('/json/:slug', routes.getRecipeJson)
 
-app.put('/', routes.replaceRecipe);
-app.post('/', routes.importBeerXml);
-app.get('/:slug', routes.readRecipe);
-app.del('/:slug', routes.deleteRecipe);
-app.get('/json/:slug', routes.getRecipeJson);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'))
+})
